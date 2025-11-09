6ec1cd921fc35f73cc817d3efe9f3cbca1fc3b6e
DaDark12 <188825825+DaDark12@users.noreply.github.com>
2025-11-09 14:35:34 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-jsx
 import React from 'react';
 import WatalappanLanding from './components/WatalappanLanding';
 
 export default function App() {
-return <WatalappanLanding />;
+  return <WatalappanLanding />;
 }
